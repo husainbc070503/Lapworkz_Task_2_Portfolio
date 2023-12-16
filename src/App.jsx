b8115361b31f53527bbd2d112b3d7b24fe106bca
Husain Champawala <husainchampawala@gmail.com>
2023-12-16 12:28:48 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { ThemeProvider, createTheme } from "@mui/material";
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
